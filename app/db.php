73fa96a842c76ef07cd333e1163c5b52271e68c6
ulyare <149316996+ulyare@users.noreply.github.com>
2023-10-31 16:02:51 +0300

    Update db.php

--- app/db.php
+++ app/db.php
@@ -1,4 +1,4 @@
 <?php
-$db = mysqli_connect('crudapp_db_1', 'root', '1', 'db') or die ('Ошибка');
+$db = mysqli_connect('db', 'root', '1', 'db') or die ('Ошибка');
 $db -> query( "SET CHARSET utf8");
 ?>
